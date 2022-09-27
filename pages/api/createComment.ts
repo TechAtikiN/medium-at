41a0e75796e857850f57c1d128aa858482625d3c
Nikita Khabya <nikitakhabya03@gmail.com>
2022-09-27 21:54:49 +0530

    modified Comments

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -5,7 +5,7 @@ const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     useCdn: process.env.NODE_ENV === "production",
-    token:process.env.SANITY_API_TOKEN
+    token:process.env.NEXT_PUBLIC_SANITY_API_TOKEN
 }
 const client=sanityClient(config)
 export default async function createComment(
